a0635a00a1e81ab667e4129143309b6786c72727
HarshVdhyani <53589353+HarshVdhyani@users.noreply.github.com>
2020-01-16 13:48:02 +0530

    Add files via upload

---
+++ All in one security plugin main file.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: All in one Security Plugin with Whitelabel Features
+ * Plugin URI: 
+ * Description: With this plugin you get WhiteLabel and Security packed features.
+ * Version: 1.0
+ * Author: Harshvardhan Dhyani
+ * Author URI:
+ */
+ 
+ include( dirname( __FILE__ ) . '/change-admin-logos.php');
+ 
+
+
+
